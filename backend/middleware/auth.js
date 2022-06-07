dcd3ad08674693d16d0a4964c51cd27af9160879
Verynicedev <dupuis.e@live.fr>
2022-06-07 12:56:29 +0200

    update .env TOKEN_SECRET dans userController et auth

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -3,9 +3,9 @@ const jwt = require('jsonwebtoken'); // Package qui sert a vérifier les tokens
 
 module.exports = (req, res, next) => {
     try {
-      const token = req.headers.authorization.split(' ')[1];          // Permet de récupérer un tableau avec les éléments "Bearer" & "Token"
-      const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');  // Permet de vérifier le token a l'aide de la clé secrète
-      const userId = decodedToken.userId;                             // Si userId & token correspondent se connecte sinon retourne une erreur
+      const token = req.headers.authorization.split(' ')[1];       // Permet de récupérer un tableau avec les éléments "Bearer" & "Token"
+      req.token = jwt.verify(token, process.env.TOKEN_SECRET);     // Permet de vérifier le token a l'aide de la clé secrète
+      const userId = req.token.userId;                             // Si userId & token correspondent se connecte sinon retourne une erreur
       req.auth = { userId };                                          
       if (req.body.userId && req.body.userId !== userId) {
         throw 'User ID non valable';
